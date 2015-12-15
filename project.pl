1ea81c5a08715f7fc2945db71bad060ac37e2c1d
rami-khalil <thesavagekoder@gmail.com>
2015-12-15 23:18:37 +0200

    Meal planner geddan

--- project.pl
+++ project.pl
@@ -23,7 +23,7 @@ plan(Goal, MealsPerDay, Weight, FatPerc, ActivityVariable, Schedule):-
 	    MinProtein, MinFat, MinCarbs,
 	    MaxProtein, MaxFat, MaxCarbs,
 	    Calories, MealsPerDay, Schedule,
-	    0, 1, [], []).
+	    3, 7, [], []).
 
 calories(bulk, TEE, CAL):-
 	CAL is TEE * 1.2.
@@ -80,11 +80,18 @@ plan(
     Weeks, Days, WACC, SACC):-
 	Days > 0,
 	% Plan for today
+	CPM is integer((1.05*CAL)/MPD),
+	MinProtein2 is integer(MinProtein/MPD),
+	MaxProtein2 is integer(MaxProtein/MPD),
+	MinFat2 is integer(MinFat/MPD),
+	MaxFat2 is integer(MaxFat/MPD),
+	MinCarbs2 is integer(MinCarbs/MPD),
+	MaxCarbs2 is integer(MaxCarbs/MPD),
 	today(
 	    0, 0, 0, 0,
-	    MinProtein, MinFat, MinCarbs,
-	    MaxProtein, MaxFat, MaxCarbs,
-	    CAL, MPD, [], Today),
+	    MinProtein2, MinFat2, MinCarbs2,
+	    MaxProtein2, MaxFat2, MaxCarbs2,
+	    CPM, MPD, [], Today),
 	%%%%%%%%%%%%%%%%%%%%%thirdMealRule(Today, WACC),
 	RD is Days - 1,
 	% Continue Week
@@ -94,21 +101,22 @@ plan(
 	    CAL, MPD, Schedule,
 	    Weeks, RD, [Today|WACC], SACC).
 
-% Today/10 plan complete. Check if it satisfies needs.
+% Today/10 plan complete. Check if it satisfies needs and dump
+% accumulators.
 today(
     Protein, Fat, Carbs, Cals,
     MinProtein, MinFat, MinCarbs,
     MaxProtein, MaxFat, MaxCarbs,
-    CAL, 0, ACC, ACC):-
-	Protein #>= MinProtein,
-	Protein #=< MaxProtein,
-	Fat #>= MinFat,
-	Fat #=< MaxFat,
-	Carbs #>= MinCarbs,
-	Carbs #=< MaxCarbs,
-	MAXCAL is integer(CAL * 1.05),
-	Cals #=< MAXCAL,
-	label([Protein, Fat, Carbs, Cals]).
+    CAL, 0, ACC, ACC).
+%	Protein #>= MinProtein,
+%	Protein #=< MaxProtein,
+%	Fat #>= MinFat,
+%	Fat #=< MaxFat,
+%	Carbs #>= MinCarbs,
+%	Carbs #=< MaxCarbs,
+%	MAXCAL is integer(CAL * 1.05),
+%	Cals #=< MAXCAL,
+%	label([Protein, Fat, Carbs, Cals]).
 
 % Today/10 plan
 today(
@@ -117,7 +125,12 @@ today(
     MaxProtein, MaxFat, MaxCarbs,
     CAL, Meals, ACC, RES):-
 	Meals > 0,
-	meal(TP, TF, TC, TJ, Meals, Components),
+	meal(
+	    TP, TF, TC, TJ,
+	    MinProtein, MinFat, MinCarbs,
+	    MaxProtein, MaxFat, MaxCarbs,
+	    CAL,
+	    Meals, Components),
 	NP #= Protein + TP,
 	NF #= Fat + TF,
 	NC #= Carbs + TC,
@@ -130,27 +143,31 @@ today(
 	    CAL, NM, [Components|ACC], RES).
 
 % Meal/10 for right now
-meal(P, F, C, J, M, Components):-
+meal(P, F, C, J, MinP, MinF, MinC, MaxP, MaxF, MaxC, MaxJ, M, Components):-
 	component(E1, P1, C1, F1, J1, _, H1, S1, B1),
-	%\+member(M, H1),
+	\+member(M, H1),
 	component(E2, P2, C2, F2, J2, _, H2, S2, B2),
-	%\+member(M, H2),
-	(   E2 = empty ; (E2 \= E1)),
+	\+member(M, H2),
+	(   E2 = empty ; (E2 \= E1)), E2 @< E1,
 	component(E3, P3, C3, F3, J3, _, H3, S3, B3),
-	%\+member(M, H3),
-	(   E3 = empty ; (E3 \= E1, E3 \= E2)),
+	\+member(M, H3),
+	(   E3 = empty ; (E3 \= E1, E3 \= E2)), E3 @< E2,
 	component(E4, P4, C4, F4, J4, _, H4, S4, B4),
-	%\+member(M, H4),
-	(   E4 = empty ; (E4 \= E1, E4 \= E2, E4 \= E3)),
+	\+member(M, H4),
+	(   E4 = empty ; (E4 \= E1, E4 \= E2, E4 \= E3)), E4 @< E3,
 	M1 in S1..B1,
 	M2 in S2..B2,
 	M3 in S3..B3,
 	M4 in S4..B4,
 	P #= M1*P1 + M2*P2 + M3*P3 + M4*P4,
-	C #= M1*C1 + M2*C2 + M3*C3 + M4*C4,
 	F #= M1*F1 + M2*F2 + M3*F3 + M4*F4,
+	C #= M1*C1 + M2*C2 + M3*C3 + M4*C4,
 	J #= M1*J1 + M2*J2 + M3*J3 + M4*J4,
-	%P #=< MP, F #=< MF, C #=< MC, J #=< MJ,
+	P #>= MinP,	P #=< MaxP,
+	F #>= MinF,	F #=< MaxF,
+	C #>= MinC,	C #=< MaxC,
+	J #=< MaxJ,
+	label([P, F, C, J]),
 	Components = [eat(E1, M1), eat(E2, M2), eat(E3, M3), eat(E4, M4)].
 
 % Component(Name, Protein, Carbs, Fats, Calories, Pref in, Hate in, Min
