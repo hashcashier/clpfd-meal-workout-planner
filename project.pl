700b4648d8a8dcba7ecb10ad682babf52e10059f
rami-khalil <thesavagekoder@gmail.com>
2015-12-15 19:42:28 +0200

    Malhash lazma:

--- project.pl
+++ project.pl
@@ -18,11 +18,12 @@ plan(Goal, MealsPerDay, Weight, FatPerc, ActivityVariable, Schedule):-
 	MaxProtein is integer(Protein * 1050),
 	MaxFat is integer(Fat * 1050),
 	MaxCarbs is integer(Carbs * 1050),
+	Calories is integer(CAL),
 	plan( % Plan/10 for 4 weeks
 	    MinProtein, MinFat, MinCarbs,
 	    MaxProtein, MaxFat, MaxCarbs,
-	    CAL, MealsPerDay, Schedule,
-	    4, 7, [], []).
+	    Calories, MealsPerDay, Schedule,
+	    0, 1, [], []).
 
 calories(bulk, TEE, CAL):-
 	CAL is TEE * 1.2.
@@ -78,7 +79,6 @@ plan(
     CAL, MPD, Schedule,
     Weeks, Days, WACC, SACC):-
 	Days > 0,
-	Weeks > 0,
 	% Plan for today
 	today(
 	    0, 0, 0, 0,
@@ -106,7 +106,7 @@ today(
 	Fat =< MaxFat,
 	Carbs >= MinCarbs,
 	Carbs =< MaxCarbs,
-	Cals =< CAL * 1.05.
+	Cals =< integer(CAL * 1.05).
 
 % Today/10 plan
 today(
@@ -119,13 +119,13 @@ today(
 	RF is MaxFat - Fat,
 	RC is MaxCarbs - Carbs,
 	RJ is CAL - Cals,
-	meal(TP, TF, TC, TJ, RP, RF, RC, RJ, M, Components),
+	meal(TP, TF, TC, TJ, RP, RF, RC, RJ, Meals, Components),
 	NP is Protein + TP,
 	NF is Fat + TF,
 	NC is Carbs + TC,
 	NJ is Cals + TJ,
-	NM is M-1,
-	plan(
+	NM is Meals-1,
+	today(
 	    NP, NF, NC, NJ,
 	    MinProtein, MinFat, MinCarbs,
 	    MaxProtein, MaxFat, MaxCarbs,
@@ -137,27 +137,55 @@ meal(P, F, C, J, MP, MF, MC, MJ, M, Components):-
 	%\+member(M, H1),
 	component(E2, P2, C2, F2, J2, _, H2, S2, B2),
 	%\+member(M, H2),
-	%E2 \= E1,
+	E2 \= E1,
 	component(E3, P3, C3, F3, J3, _, H3, S3, B3),
 	%\+member(M, H3),
-	%E3 \= E1, E3 \= E2,
+	E3 \= E1, E3 \= E2,
 	component(E4, P4, C4, F4, J4, _, H4, S4, B4),
 	%\+member(M, H4),
-	%E4 \= E1, E4 \= E2, E4 \= E3,
+	E4 \= E1, E4 \= E2, E4 \= E3,
 	M1 in S1..B1, M2 in S2..B2, M3 in S3..B3, M4 in S4..B4,
 	P #= M1*P1 + M2*P2 + M3*P3 + M4*P4,
 	C #= M1*C1 + M2*C2 + M3*C3 + M4*C4,
 	F #= M1*F1 + M2*F2 + M3*F3 + M4*F4,
-	J is M1*J1 + M2*J2 + M3*J3 + M4*J4,
-	P #=< MP, F #=< MF, C #=< MC, J #=< MJ,
+	J #= M1*J1 + M2*J2 + M3*J3 + M4*J4,
+	P #=< MP, F #=< MF, C #=< MC,%%%%%%%%% J #=< MJ,
 	label([M1, M2, M3, M4]),
 	Components = [eat(E1, M1), eat(E2, M2), eat(E3, M3), eat(E4, M4)].
 
 % Component(Name, Protein, Carbs, Fats, Calories, Pref in, Hate in, Min
 % units, max units)
 
-component(empty, 0, 0, 0, 0, [], [], 1, 1).
-component(banana, 1.1, 0.3, 23, 89, [], [], 1, 2).
+component(empty,			0,	0,	0,	0,	[], [], 1, 1).
+component(banana,			1100,	300,	23000,	89,	[], [], 1, 10).
+component(broccoli,			2800,	7000,	400,	34,	[], [], 1, 10).
+component(carrots,			900,	10000,	200,	41,	[], [], 1, 10).
+component(cheddar_cheese,		25000,	1300,	33000,	402,	[], [], 1, 10).
+component(chicken_breast_uncooked,	21200,	0,	2500,	114,	[], [], 1, 10).
+component(egg_whites_uncooked,		11000,	700,	200,	52,	[], [], 1, 10).
+component(fish,				19000,	0,	6000,	134,	[], [], 1, 10).
+component(gouda_cheese,			25000,	2200,	27000,	356,	[], [], 1, 10).
+component(honey,			300,	82000,	0,	304,	[], [], 1, 10).
+component(lean_beef_uncooked,		20000,	0,	6000,	137,	[], [], 1, 10).
+component(milk_full_fat,		3300,	4600,	3700,	64,	[], [], 1, 10).
+component(multi_grain_bread,		13000,	43000,	4200,	265,	[], [], 1, 10).
+component(olive_oil,			0,	0,	100000,	884,	[], [], 1, 10).
+component(pasta_uncooked,		13000,	75000,	1500,	371,	[], [], 1, 10).
+component(peanut_butter,		25000,	20000,	50000,	588,	[], [], 1, 10).
+component(pear,				400,	15000,	100,	57,	[], [], 1, 10).
+component(potato_uncooked,		2000,	17000,	100,	77,	[], [], 1, 10).
+component(salmon_uncooked,		20000,	0,	13000,	208,	[], [], 1, 10).
+component(shrimp_uncooked,		20000,	0,	500,	85,	[], [], 1, 10).
+component(skimmed_milk,			3400,	5000,	100,	34,	[], [], 1, 10).
+component(spinach,			2900,	3600,	400,	23,	[], [], 1, 10).
+component(sweet_potato_uncooked,	1600,	20000,	0,	86,	[], [], 1, 10).
+component(swiss_cheese,			27000,	5000,	28000,	380,	[], [], 1, 10).
+component(tomato,			900,	3900,	200,	18,	[], [], 1, 10).
+component(tuna_brine,			26000,	0,	1000,	116,	[], [], 1, 10).
+component(whey_protein,			80000,	10000,	3300,	400,	[], [], 1, 10).
+component(white_rice_uncoooked,		7000,	82000,	600,	370,	[], [], 1, 10).
+component(whole_egg_uncooked,		13000,	700,	10000,	143,	[], [], 1, 10).
+
 
 noWeeksRep(A, B):-
 	\+member(A, B).
