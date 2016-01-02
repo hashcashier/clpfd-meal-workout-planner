37c97b13c58eb65b7c9baf9c11acaec2db1c470f
rami-khalil <thesavagekoder@gmail.com>
2016-01-02 12:49:58 +0200

    Before Everything Is Destroyed

--- project.pl
+++ project.pl
@@ -1,5 +1,6 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
+:- use_module(library(apply)).
 
 % Plan/6 for a month
 plan(Goal, MealsPerDay, Weight, FatPerc, ActivityVariable, Schedule):-
@@ -19,11 +20,12 @@ plan(Goal, MealsPerDay, Weight, FatPerc, ActivityVariable, Schedule):-
 	MaxFat is integer(Fat * 1050),
 	MaxCarbs is integer(Carbs * 1050),
 	Calories is integer(CAL),
+	writeln(Calories),
 	plan( % Plan/10 for 4 weeks
 	    MinProtein, MinFat, MinCarbs,
 	    MaxProtein, MaxFat, MaxCarbs,
 	    Calories, MealsPerDay, Schedule,
-	    3, 7, [], []).
+	    0, 1, [], []).
 
 calories(bulk, TEE, CAL):-
 	CAL is TEE * 1.2.
@@ -107,16 +109,10 @@ today(
     Protein, Fat, Carbs, Cals,
     MinProtein, MinFat, MinCarbs,
     MaxProtein, MaxFat, MaxCarbs,
-    CAL, 0, ACC, ACC).
-%	Protein #>= MinProtein,
-%	Protein #=< MaxProtein,
-%	Fat #>= MinFat,
-%	Fat #=< MaxFat,
-%	Carbs #>= MinCarbs,
-%	Carbs #=< MaxCarbs,
-%	MAXCAL is integer(CAL * 1.05),
-%	Cals #=< MAXCAL,
-%	label([Protein, Fat, Carbs, Cals]).
+    CAL, 0, ACC, ACC):-
+	extractComponents(ACC, Raw),
+	noRepeatedMeals(Raw),
+	obeyDailyLimit(Raw).
 
 % Today/10 plan
 today(
@@ -144,17 +140,17 @@ today(
 
 % Meal/10 for right now
 meal(P, F, C, J, MinP, MinF, MinC, MaxP, MaxF, MaxC, MaxJ, M, Components):-
-	component(E1, P1, C1, F1, J1, _, H1, S1, B1),
+	component(E1, P1, C1, F1, J1, _, H1, S1, B1, _),
 	\+member(M, H1),
-	component(E2, P2, C2, F2, J2, _, H2, S2, B2),
+	component(E2, P2, C2, F2, J2, _, H2, S2, B2, _),
 	\+member(M, H2),
-	(   E2 = empty ; (E2 \= E1)), E2 @< E1,
-	component(E3, P3, C3, F3, J3, _, H3, S3, B3),
+	(   E2 = empty ; (E2 @< E1, E2 \= E1)),
+	component(E3, P3, C3, F3, J3, _, H3, S3, B3, _),
 	\+member(M, H3),
-	(   E3 = empty ; (E3 \= E1, E3 \= E2)), E3 @< E2,
-	component(E4, P4, C4, F4, J4, _, H4, S4, B4),
+	(   E3 = empty ; (E3 @< E2, E3 \= E1, E3 \= E2)), 
+	component(E4, P4, C4, F4, J4, _, H4, S4, B4, _),
 	\+member(M, H4),
-	(   E4 = empty ; (E4 \= E1, E4 \= E2, E4 \= E3)), E4 @< E3,
+	(   E4 = empty ; (E4 @< E3, E4 \= E1, E4 \= E2, E4 \= E3)),
 	M1 in S1..B1,
 	M2 in S2..B2,
 	M3 in S3..B3,
@@ -170,59 +166,92 @@ meal(P, F, C, J, MinP, MinF, MinC, MaxP, MaxF, MaxC, MaxJ, M, Components):-
 	label([P, F, C, J]),
 	Components = [eat(E1, M1), eat(E2, M2), eat(E3, M3), eat(E4, M4)].
 
-% Component(Name, Protein, Carbs, Fats, Calories, Pref in, Hate in, Min
-% units, max units)
-
-component(empty,			0,	0,	0,	0,	[],		[],	1, 1).
-component(banana,			1100,	300,	23000,	89,	[],		[],	1, 2).
-component(broccoli,			2800,	7000,	400,	34,	[],		[],	1, 2).
-component(carrots,			900,	10000,	200,	41,	[],		[],	1, 2).
-component(cheddar_cheese,		25000,	1300,	33000,	402,	[1,2,5],	[],	1, 1).
-component(chicken_breast_uncooked,	21200,	0,	2500,	114,	[3],		[1],	1, 2).
-component(egg_whites_uncooked,		11000,	700,	200,	52,	[],		[],	1, 2).
-component(fish,				19000,	0,	6000,	134,	[3],		[1],	1, 2).
-component(gouda_cheese,			25000,	2200,	27000,	356,	[1,2,5],	[],	1, 1).
-component(honey,			300,	82000,	0,	304,	[],		[],	1, 1).
-component(lean_beef_uncooked,		20000,	0,	6000,	137,	[3],		[1],	1, 2).
-component(milk_full_fat,		3300,	4600,	3700,	64,	[],		[],	1, 1).
-component(multi_grain_bread,		13000,	43000,	4200,	265,	[],		[],	1, 2).
-component(olive_oil,			0,	0,	100000,	884,	[],		[],	1, 1).
-component(pasta_uncooked,		13000,	75000,	1500,	371,	[3],		[1],	1, 1).
-component(peanut_butter,		25000,	20000,	50000,	588,	[],		[],	1, 1).
-component(pear,				400,	15000,	100,	57,	[],		[],	1, 2).
-component(potato_uncooked,		2000,	17000,	100,	77,	[],		[],	1, 2).
-component(salmon_uncooked,		20000,	0,	13000,	208,	[3],		[1],	1, 2).
-component(shrimp_uncooked,		20000,	0,	500,	85,	[3],		[1],	1, 2).
-component(skimmed_milk,			3400,	5000,	100,	34,	[],		[],	1, 2).
-component(spinach,			2900,	3600,	400,	23,	[],		[1],	1, 3).
-component(sweet_potato_uncooked,	1600,	20000,	0,	86,	[],		[],	1, 2).
-component(swiss_cheese,			27000,	5000,	28000,	380,	[1,2,5],	[],	1, 1).
-component(tomato,			900,	3900,	200,	18,	[],		[],	1, 4).
-component(tuna_brine,			26000,	0,	1000,	116,	[],		[1],	1, 2).
-component(whey_protein,			80000,	10000,	3300,	400,	[],		[],	1, 1).
-component(white_rice_uncoooked,		7000,	82000,	600,	370,	[],		[],	1, 1).
-component(whole_egg_uncooked,		13000,	700,	10000,	143,	[1],		[],	1, 3).
+% Component(Name, 					Protein, Carbs, Fats, 	Cals, 	Pref in, 	Hate in, 	Min units, 	Max units, Limit)
+component(empty,					0,		0,		0,		0,		[],			[],			1, 			1, 			2).
+component(banana,					1100,	300,	23000,	89,		[],			[],			1, 			2, 			2).
+component(broccoli,					2800,	7000,	400,	34,		[],			[],			1, 			2, 			2).
+component(carrots,					900,	10000,	200,	41,		[],			[],			1, 			2, 			2).
+component(cheddar_cheese,			25000,	1300,	33000,	402,	[1,2,5],	[],			1, 			1, 			2).
+component(chicken_breast_uncooked,	21200,	0,		2500,	114,	[3],		[1],		1, 			2, 			2).
+component(egg_whites_uncooked,		11000,	700,	200,	52,		[],			[],			1, 			2, 			2).
+component(fish,						19000,	0,		6000,	134,	[3],		[1],		1, 			2, 			2).
+component(gouda_cheese,				25000,	2200,	27000,	356,	[1,2,5],	[],			1, 			1, 			2).
+component(honey,					300,	82000,	0,		304,	[],			[],			1, 			1, 			2).
+component(lean_beef_uncooked,		20000,	0,		6000,	137,	[3],		[1],		1, 			2, 			2).
+component(milk_full_fat,			3300,	4600,	3700,	64,		[],			[],			1, 			1, 			2).
+component(multi_grain_bread,		13000,	43000,	4200,	265,	[],			[],			1, 			2, 			2).
+component(olive_oil,				0,		0,		100000,	884,	[],			[],			1, 			1, 			2).
+component(pasta_uncooked,			13000,	75000,	1500,	371,	[3],		[1],		1, 			1, 			2).
+component(peanut_butter,			25000,	20000,	50000,	588,	[],			[],			1, 			1, 			2).
+component(pear,						400,	15000,	100,	57,		[],			[],			1, 			2, 			2).
+component(potato_uncooked,			2000,	17000,	100,	77,		[],			[],			1, 			2, 			2).
+component(salmon_uncooked,			20000,	0,		13000,	208,	[3],		[1],		1, 			2, 			2).
+component(shrimp_uncooked,			20000,	0,		500,	85,		[3],		[1],		1, 			2, 			2).
+component(skimmed_milk,				3400,	5000,	100,	34,		[],			[],			1, 			2, 			2).
+component(spinach,					2900,	3600,	400,	23,		[],			[1],		1, 			3, 			2).
+component(sweet_potato_uncooked,	1600,	20000,	0,		86,		[],			[],			1, 			2, 			2).
+component(swiss_cheese,				27000,	5000,	28000,	380,	[1,2,5],	[],			1, 			1, 			2).
+component(tomato,					900,	3900,	200,	18,		[],			[],			1, 			4, 			4).
+component(tuna_brine,				26000,	0,		1000,	116,	[],			[1],		1, 			2, 			2).
+component(whey_protein,				80000,	10000,	3300,	400,	[],			[],			1, 			1, 			2).
+component(white_rice_uncoooked,		7000,	82000,	600,	370,	[],			[],			1, 			1, 			2).
+component(whole_egg_uncooked,		13000,	700,	10000,	143,	[1],		[],			1, 			3, 			3).
 
 
 noWeeksRep(A, B):-
 	\+member(A, B).
 
-thirdMealRule([_|[_|[ThirdMeal|_]]], Meals):-
-	thirdMealRule(ThirdMeal, Meals, N),
-	N < 3.
-thirdMealRule(_, [], 0).
-thirdMealRule(M, [H|T], N):-
-	sameMeal(M, H),
-	N1 is N+1,
-	thirdMealRule(M, T, N1).
-thirdMealRule(M, [H|T], N):-
-	\+sameMeal(M, H),
-	thirdMealRule(M, T, N).
+%thirdMealRule([_|[_|[ThirdMeal|_]]], Meals):-
+%	thirdMealRule(ThirdMeal, Meals, N),
+%	N < 3.
+%thirdMealRule(_, [], 0).
+%thirdMealRule(M, [H|T], N):-
+%	sameMeal(M, H),
+%	N1 is N+1,
+%	thirdMealRule(M, T, N1).
+%thirdMealRule(M, [H|T], N):-
+%	\+sameMeal(M, H),
+%	thirdMealRule(M, T, N).
 
-sameMeal(meal(X, A), meal(X, B)):-
-	true.
+%sameMeal(meal(X, A), meal(X, B)):-
+%	true.
+
 %Write Meal Comparator
-%Change GRAMS to Milligrams and use CLPFD
+
+extractComponents(Eat, Raw):-
+	foldr(extractComponents, Eat, [], Raw).
+extractComponents(Eat, T, [H|T]):-
+	foldr(extractComponentNames, Eat, [], H).
+extractComponentNames(eat(Name, _), T, [Name|T]).
+
+noRepeatedMeals([]).
+noRepeatedMeals([H|T]):-
+	maplist(notSameMeal(H), T).
+notSameMeal(A, B):-
+	\+sameMeal(A, B).
+sameMeal(A, B):-
+	list_to_set(A, AS), list_to_set(B, BS),
+	subset(AS, BS), subset(BS, AS).
+obeyDailyLimit(Raw):-
+	flatten(Raw, Flat),
+	list_to_set(Flat, Set),
+	maplist(obeyDailyLimit(Flat), Set).
+obeyDailyLimit(List, Elem):-
+	component(Elem, _, _, _, _, _, _, _, _, Limit),
+	count(Elem, List, Count),
+	Count =< Limit.
+
+count(_, [], 0).
+count(E, [E|T], N):-
+	count(E, T, N1),
+	N is N1 + 1.
+count(E, [H|T], N):-
+	H \= E,
+	count(E, T, N).
+
+foldr(Goal, List, V0, V):-
+	reverse(List, RList),
+	foldl(Goal, RList, V0, V).
 
 
 
