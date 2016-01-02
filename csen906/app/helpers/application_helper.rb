a708dc84718359abb0c7deeeaecdd8f8c2a1fa6b
MohamedBassem <medoox240@gmail.com>
2016-01-02 23:30:44 +0200

    Adding prolog solver function

--- csen906/app/helpers/application_helper.rb
+++ csen906/app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
 
+  PROLOG_COMMAND = "prolog"
+
+  SCRIPT_DIR = './scripts/'
+  EXERCISE_FILE = 'exercise.pl'
+
+  def self.solve_prolog(consult_script, query, print)
+    script_file = SCRIPT_DIR + consult_script
+    writelns = print.map { |x| "writeln(" + x + ")" }.join(",")
+    `#{PROLOG_COMMAND} -s #{script_file} -g '#{query}, #{writelns}' -t 'halt'`
+  end
 
 end
