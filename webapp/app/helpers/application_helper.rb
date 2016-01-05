184fe54a0cdd1220ccc09c8eabcf5bfabfbcf3d8
MohamedBassem <medoox240@gmail.com>
2016-01-05 15:09:03 +0200

    Changing the command execution work dir

--- webapp/app/helpers/application_helper.rb
+++ webapp/app/helpers/application_helper.rb
@@ -6,13 +6,12 @@ module ApplicationHelper
   EXERCISE_FILE = 'exercise.pl'
 
   def self.solve_prolog(consult_script, query, print)
-    script_file = SCRIPT_DIR + consult_script
     writelns = print.map { |x| "writeln(" + x + ")" }.join(",")
 
     full_query = [query, writelns].join(",").chomp ","
     puts full_query
 
-    `#{PROLOG_COMMAND} -s #{script_file} -g '#{full_query}' -t 'halt'`
+    `cd #{SCRIPT_DIR} && #{PROLOG_COMMAND} -s #{consult_script} -g '#{full_query}' -t 'halt'`
   end
 
 end
